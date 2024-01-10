3956be95e33fe9254069126e4d071733a13de98d
Sivaselvan <mvs@buffalo.edu>
2024-01-10 12:48:48 -0500

    Tested swing_measure.ino with two load cell channels with Arduino IDE Serial monitor and Serial plotter

--- src/swing_measure/swing_measure.ino
+++ src/swing_measure/swing_measure.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <HX711-multi.h>
 
+#define NUMLC 2 // number of load cells
+
+// Pins for the HX711 amplifiers
+#define CLK 2 // clock pin shared across all the HX711s
+#define LC1 3 // data pin for load cell #1
+#define LC2 4 // data pin for load cell #2
+
+# define GAIN 128
+
+byte DOUTS[NUMLC] = {LC1, LC2};
+
+HX711MULTI lcs(NUMLC, DOUTS, CLK, GAIN);
+
+long int forces[NUMLC];
+
+void setup() {
+  Serial.begin(57600);
+  Serial.flush();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcs.readRaw(forces);
+  for (int i=0; i<NUMLC; ++i) {;
+    Serial.print(forces[i]);  
+    Serial.print( (i!=NUMLC-1)?"\t":"\n");
+  }
+  delay(100);
 }
