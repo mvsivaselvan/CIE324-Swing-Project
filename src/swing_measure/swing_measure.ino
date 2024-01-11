56c373f95d3772ced068d1e4c8cad42ac4c89ddc
Sivaselvan <mvs@buffalo.edu>
2024-01-11 14:33:42 -0500

    Added IMU to swing_measure.ino, just reading acceleration components for now

--- src/swing_measure/swing_measure.ino
+++ src/swing_measure/swing_measure.ino
@@ -1,5 +1,13 @@
+// For HX711
 #include <HX711-multi.h>
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/////////////////////////////////////////////////////
+// LOAD CELL-related defintions
 #define NUMLC 2 // number of load cells
 
 // Pins for the HX711 amplifiers
@@ -7,20 +15,38 @@
 #define LC1 3 // data pin for load cell #1
 #define LC2 4 // data pin for load cell #2
 
-# define GAIN 128
+# define GAIN 128 // Gain for HX711s
 
 byte DOUTS[NUMLC] = {LC1, LC2};
 
 HX711MULTI lcs(NUMLC, DOUTS, CLK, GAIN);
 
 long int forces[NUMLC];
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+// IMU-related definitions
+Adafruit_BNO055 myIMU = Adafruit_BNO055();
+
+/////////////////////////////////////////////////////
 
 void setup() {
   Serial.begin(57600);
   Serial.flush();
+
+  myIMU.begin();
+  myIMU.setExtCrystalUse(true);
 }
 
 void loop() {
+  imu::Vector<3> acc = myIMU.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  Serial.print(acc.x());
+  Serial.print(",");
+  Serial.print(acc.y());
+  Serial.print(",");
+  Serial.print(acc.z());
+  Serial.print(",");
+  
   lcs.readRaw(forces);
   for (int i=0; i<NUMLC; ++i) {;
     Serial.print(forces[i]);  
