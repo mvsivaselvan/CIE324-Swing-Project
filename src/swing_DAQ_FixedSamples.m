7f2f1882257611d4e1d429875757d6e48a47c9ec
Sivaselvan <mvs@buffalo.edu>
2024-01-11 09:51:50 -0500

    Forgotten to 'add' swing_DAQ_FinxedSamples.m in previous commit

--- src/swing_DAQ_FixedSamples.m
+++ src/swing_DAQ_FixedSamples.m
@@ -11,22 +11,11 @@ configureTerminator(myArduino,"CR/LF");
 myArduino.UserData = struct("Data",[],"Count",0);
 
 configureCallback(myArduino,"terminator",...
-                 @(src,evt)(readArduino(src,evt,numSamples)));
+                 @(src,evt)(readArduinoFixedSamples(src,evt,numSamples)));
 
 waitfor(myArduino, "BytesAvailableFcnMode", "off");
 
-% Wait for acquistion to complete
-% while (true)
-%     % problem with putting this in the while clause is it uses the value at
-%     % the time the look starts
-%     n = myArduino.UserData.Count;
-%     if (n > numSamples) 
-%         break
-%     else
-%         fprintf('%s\n',myArduino.BytesAvailableFcnMode);
-%     end
-% end
-% pause(numSamples*0.1+5);
-
 acquiredData = myArduino.UserData.Data;
 myArduino.delete
+
+figure, plot(acquiredData)
